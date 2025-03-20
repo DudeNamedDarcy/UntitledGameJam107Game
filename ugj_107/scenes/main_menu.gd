f433d94e60057fae52083c9abdd35f5496953fdf
Darcy Loane-Billings <122037471+DudeNamedDarcy@users.noreply.github.com>
2025-03-19 22:39:38 -0400

    Started Tutorial Scene

--- ugj_107/scenes/main_menu.gd
+++ ugj_107/scenes/main_menu.gd
@@ -15,3 +15,7 @@ func _physics_process(delta: float) -> void:
 
 func _on_quit_game_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_start_game_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/tutorial_scene.tscn")
